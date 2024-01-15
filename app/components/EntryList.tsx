e1f2ada6c9eeab1c4cc2f5540c1172fe0f9967ec
jyodai <tdkdtk5011@gmail.com>
2024-01-16 02:47:29 +0900

    * 型

--- app/components/EntryList.tsx
+++ app/components/EntryList.tsx
@@ -57,7 +57,7 @@ const EntryList = (props: Props) => {
                         <li key={index} className="flex justify-between items-center p-4 border-b border-gray-200 last:border-0">
                             <div className="flex-grow">
                                 <div className="text-sm text-gray-600">{dateUtils.formatDateToDateTimeLocal(entry.date)}</div>
-                                <div className="text-lg font-medium">{entry.category.name}</div>
+                                <div className="text-lg font-medium">{entry.category ? entry.category.name : 'No Category'}</div>
                                 <div className="text-gray-800">{entry.amount} 円</div>
                                 <div className="text-gray-500 italic">{entry.store}</div>
                                 <div className="text-gray-500 italic">{entry.memo}</div>
