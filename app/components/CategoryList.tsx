1c6a7caa7c5f46a7bdd28e42e985574a331a17a9
jyodai <tdkdtk5011@gmail.com>
2024-02-06 20:58:29 +0900

    * キャンセルボタンが動作していなかった

--- app/components/CategoryList.tsx
+++ app/components/CategoryList.tsx
@@ -60,7 +60,7 @@ const CategoryList = (props: Props) => {
             {editingCategory ? (
                 <>
                     <AddCategory initialCategory={editingCategory} onSave={handleSave} />
-                    <button onClick={() => setEditingEntry(null)} className="mt-4 w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-black bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <button onClick={() => setEditingCategory(null)} className="mt-4 w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-black bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                         キャンセル
                     </button>
                 </>
