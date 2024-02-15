f28be983117df2e6c770acd379471ab14d34501e
jyodai <tdkdtk5011@gmail.com>
2024-02-15 22:57:05 +0900

    * タイトルをリンクにする

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,14 @@
 'use client'
 
 import * as React from 'react';
-import Link from 'next/link'
+// import Link from 'next/link'
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
+import Link from '@mui/material/Link';
 import MenuIcon from '@mui/icons-material/Menu';
 import CreateIcon from '@mui/icons-material/Create';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
@@ -27,7 +28,9 @@ const Header = () => {
           <Toolbar>
 
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              家計簿アプリ
+              <Link color="inherit" underline="none" href="/">
+                家計簿アプリ
+              </Link>
             </Typography>
 
             <Link href="/add-entry">
