28942d4ce9f962593fde3da98f423cf3da1c2b4c
jyodai <tdkdtk5011@gmail.com>
2024-02-24 05:48:06 +0900

    * キャンセルはIconに変更

--- app/components/EntryList.tsx
+++ app/components/EntryList.tsx
@@ -67,12 +67,14 @@ const EntryList = (props: Props) => {
         <ThemeProvider theme={darkTheme}>
           <div className="p-4">
               {editingEntry ? (
-                  <>
-                      <AddEntryForm initialEntry={editingEntry} categories={categories} onSave={handleSave} />
-                      <button onClick={() => setEditingEntry(null)} className="mt-4 w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-black bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                          キャンセル
-                      </button>
-                  </>
+                <div key='editing' className="flex flex-col">
+                  <div className="flex justify-end">
+                    <ClearIcon className="cursor-pointer" onClick={(e) => setEditingEntry(null)} />
+                  </div>
+                  <div>
+                    <AddEntryForm initialEntry={editingEntry} categories={categories} onSave={handleSave} />
+                  </div>
+                </div>
               ) : (
                 <div>
                   {entries.map((entry, index) => (
